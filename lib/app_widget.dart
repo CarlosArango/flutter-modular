0f39849d53bc74ba6e017a3db3ca9c17dd62ad1a
Carlos Arango <arangotorcar@gmail.com>
2021-09-24 06:16:39 -0500

    First commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // set your initial route
+      initialRoute: "/",
+      navigatorKey: Modular.navigatorKey,
+      // add Modular to manage the routing system
+      onGenerateRoute: Modular.generateRoute,
+    );
+  }
+}
